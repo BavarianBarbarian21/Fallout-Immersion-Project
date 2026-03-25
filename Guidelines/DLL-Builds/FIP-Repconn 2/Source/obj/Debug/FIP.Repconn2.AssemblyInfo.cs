90a87e57051c6ab8284f9114b5d74250d3280038
MrHouse22 <matthias.feil@stmuk.bayern.de>
2026-03-25 17:01:38 +0100

    25.03.

--- Guidelines/DLL-Builds/FIP-Repconn 2/Source/obj/Debug/FIP.Repconn2.AssemblyInfo.cs
+++ Guidelines/DLL-Builds/FIP-Repconn 2/Source/obj/Debug/FIP.Repconn2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FIP.Repconn2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed7ecea8081fa2948edda5d5e6037e81d6c02f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bddfc7b279a9c643b1d67d5a0ca0135c7d3ab375")]
 [assembly: System.Reflection.AssemblyProductAttribute("FIP.Repconn2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FIP.Repconn2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
