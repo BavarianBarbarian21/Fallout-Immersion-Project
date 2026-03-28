3f605f217de9a9c0788c9132b4babab5d8f55f26
BavarianBarbarian21 <feil.matthias@web.de>
2026-03-28 22:31:59 +0100

    28.03.

---
+++ Guidelines/Assembler/FIP-Repconn/Source/FIP.Repconn.Malpais/BiomeWorker_Malpais.cs
@@ -0,0 +1,18 @@
+using RimWorld;
+using RimWorld.Planet;
+
+namespace FIP.Repconn;
+
+public sealed class BiomeWorker_Malpais : BiomeWorker
+{
+    public override float GetScore(BiomeDef biome, Tile tile, PlanetTile planetTile)
+    {
+        return tile != null
+            && !tile.WaterCovered
+            && tile.hilliness is Hilliness.Mountainous or Hilliness.Impassable
+            && tile.temperature > 26f
+            && tile.temperature <= 32f
+            ? 18f
+            : -100f;
+    }
+}
