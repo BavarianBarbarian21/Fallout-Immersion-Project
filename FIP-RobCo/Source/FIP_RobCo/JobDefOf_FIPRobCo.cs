acd6395d8a8d7fef9a31348bfa9f824d5f9c99bc
farxmai <farxmai2@gmail.com>
2026-04-02 23:25:01 +0200

    add source files

---
+++ FIP-RobCo/Source/FIP_RobCo/JobDefOf_FIPRobCo.cs
@@ -0,0 +1,17 @@
+using RimWorld;
+using Verse;
+
+namespace FIP_RobCo
+{
+    [DefOf]
+    public static class JobDefOf_FIPRobCo
+    {
+        public static JobDef ReloadAbilityFromMap;
+        public static JobDef ReloadMechAbility;
+
+        static JobDefOf_FIPRobCo()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(JobDefOf_FIPRobCo));
+        }
+    }
+}
